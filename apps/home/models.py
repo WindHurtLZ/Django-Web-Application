eadaf9e572627a8e365207e5ded9ccfad305b35f
Xufan Lin <xml5282@psu.edu>
2024-09-09 01:32:26 -0400

    feat: create device model

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Device(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+        ('updating', 'Updating'),
+    ]
+
+    name = models.CharField(max_length=100)
+    date = models.DateField(autonow_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    version = models.CharField(max_length=50)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+    battery = models.IntegerField(default=0)
+    location = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s: %s" % (self.owner, self.name)
+
