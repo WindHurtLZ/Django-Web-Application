8cae17fa6726be860d3b8bb762f209819833e9b1
Xufan Lin <xml5282@psu.edu>
2024-09-10 15:48:49 -0400

    feat: bind models in admin panel

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from apps.home.models import Device
+
+
 # Register your models here.
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'owner', 'version', 'status', 'battery', 'location', 'type')
