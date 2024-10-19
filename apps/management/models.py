56689ae6fa48f50f410d89c75c17a664e399452e
Xufan Lin <xml5282@psu.edu>
2024-10-18 23:09:13 -0400

    change: new field "asset_number" corresponds to 4-digit number on device's asset that user will use it to distinguish between same devices.

--- apps/management/models.py
+++ apps/management/models.py
@@ -21,6 +21,28 @@ class Device(models.Model):
     battery = models.IntegerField(default=0)
     location = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
+    asset_number = models.CharField(max_length=4, unique=True)
+
+    def __str__(self):
+        return "%s: %s" % (self.owner, self.name)
+
+    def save(self, *args, **kwargs):
+        if not self.asset_number:
+            self.asset_number = self.generate_asset_number()
+        super(Device, self).save(*args, **kwargs)
+
+    @staticmethod
+    def generate_asset_number():
+        # Search last asset number
+        last_device = Device.objects.filter(asset_number__isnull=False).order_by('-asset_number').first()
+        if last_device:
+            last_number = int(last_device.asset_number)
+            new_number = last_number + 1
+        else:
+            new_number = 0
+
+        # formate to 4 digit
+        return f"{new_number:04d}"
 
     def __str__(self):
         return "%s: %s" % (self.owner, self.name)
