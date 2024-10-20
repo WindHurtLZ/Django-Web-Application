8d0ffb0c5d4253f453b6aeadedbdc4da2c05f178
Xufan Lin <xml5282@psu.edu>
2024-10-20 03:48:39 -0400

    feat: full functionality for lock control system for user side and device side.

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('ride-1/', views.ride1_view, name='ride-1'),
     path('ride-2/', views.ride2_view, name='ride-2'),
     path('ride-3/', views.ride3_view, name='ride-3'),
+    path('ride-4/', views.ride4_view, name='ride-4'),
 
     path('validate-bike-number/', views.validate_bike_number, name='validate-bike-number'),
+    path('get-user-ride-info/', views.get_user_ride_info, name='get-user-ride-info'),
+    path('end-ride/', views.end_ride, name='end-ride'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,9 +1,13 @@
 import json
+from importlib.metadata import requires
 
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.views.decorators.http import require_POST
 
+from apps.home.models import Ride
 from apps.management.models import Device
 from apps.management.onem2m_service import update_lock_module
 
@@ -34,9 +38,35 @@ def ride3_view(request):
     originator = f"C{device.type}_{str(device.ae_id)[:8]}"
     update_lock_module(ae_rn, originator, status=False)
 
+    # Create a new ride record
+    new_ride = Ride.objects.create(
+        user=request.user,
+        device=device,
+        start_time=timezone.now()
+    )
+    new_ride.save()
+
+    device.status = 'active'
+    device.save()
+
     request.session.pop('bike_number', None)
     return render(request, 'home/ride-3.html')
 
+
+def ride4_view(request):
+    ride = Ride.objects.filter(user=request.user).order_by('-start_time').first()
+
+    if ride and ride.end_time:
+        duration_seconds = (ride.end_time - ride.start_time).total_seconds()
+        hours, remainder = divmod(duration_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        formatted_duration = f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
+    else:
+        formatted_duration = "00:00:00"
+
+    return render(request, 'home/ride-4.html', {'ride_duration': formatted_duration})
+
+@login_required(login_url="/login/")
 def validate_bike_number(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -56,3 +86,53 @@ def validate_bike_number(request):
 
     return JsonResponse({'valid': False})
 
+
+@login_required(login_url="/login/")
+def get_user_ride_info(request):
+    user = request.user
+    latest_ride = user.rides.order_by('-start_time').first()
+
+    if latest_ride and latest_ride.end_time is None:
+        # current ride
+        duration_seconds = (timezone.now() - latest_ride.start_time).total_seconds()
+        data = {
+            'device_name': latest_ride.device.name,
+            'status': 'in_use',
+            'duration_seconds': duration_seconds,
+            'ride_history': list(user.rides.all().values())
+        }
+    else:
+        # No riding
+        data = {
+            'status': 'no_active_ride',
+            'ride_history': list(user.rides.all().values())  # Return all user's ride
+        }
+
+    return JsonResponse(data)
+
+@login_required(login_url="/login/")
+@require_POST
+def end_ride(request):
+    # Fetch Newest Ride Record
+    latest_ride = request.user.rides.filter(end_time__isnull=True).order_by('-start_time').first()
+
+    if latest_ride:
+        device = latest_ride.device
+        ae_rn = device.ae_rn
+        originator = f"C{device.type}_{str(device.ae_id)[:8]}"
+
+        # Lock Bike
+        update_lock_module(ae_rn, originator, status=True)
+
+        # Save end time for record
+        latest_ride.end_time = timezone.now()
+        latest_ride.save()
+
+        # Set device current user to None for next use
+        device.user = None
+        device.status = 'inactive'
+        device.save()
+
+        return redirect('ride-4')
+
+    return redirect('home')
