d90f6af5a3400dfe14645556146afe6f62236b28
Xufan Lin <xml5282@psu.edu>
2024-10-20 03:46:02 -0400

    feat: A new model Ride that record the ride history data for each user

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from apps.home.models import Ride
+
+
 # Register your models here.
+@admin.register(Ride)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'device', 'start_time', 'end_time')
--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from apps.management.models import Device
+
+
+class Ride(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rides')
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='rides')
+    start_time = models.DateTimeField(auto_now_add=True)  # Ride Start Time
+    end_time = models.DateTimeField(null=True, blank=True)  # End Time
+
+    def get_duration(self):
+        if self.end_time:
+            return (self.end_time - self.start_time).total_seconds()
+        else:
+            from django.utils import timezone
+            return (timezone.now() - self.start_time).total_seconds()
+
+    def __str__(self):
+        return f"Ride by {self.user} on {self.device.name}"
