5848bcd65b82ab032a59f59c7f10e30df7e13cce
Xufan Lin <xml5282@psu.edu>
2024-10-30 22:00:23 -0400

    feat: new data model for each module in AE

--- apps/management/admin.py
+++ apps/management/admin.py
@@ -6,4 +6,4 @@ from apps.management.models import Device
 # Register your models here.
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('ae_id', 'ae_rn', 'name', 'date', 'owner', 'version', 'status', 'battery', 'location', 'type', 'asset_number')
+    list_display = ('ae_id', 'ae_rn', 'hardware_id', 'asset_number', 'name', 'date', 'owner', 'user', 'version', 'status', 'location', 'type')
--- apps/widgets/admin.py
+++ apps/widgets/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from apps.widgets.models import DeviceData
+from apps.widgets.models import DeviceData, MeshConnectivity, Battery
 
 
 # Register your models here.
 @admin.register(DeviceData)
 class DeviceDataAdmin(admin.ModelAdmin):
     list_display = ('device', 'latitude', 'longitude', 'temperature', 'speed', 'timestamp')
+
+@admin.register(MeshConnectivity)
+class MeshConnectivityAdmin(admin.ModelAdmin):
+    list_display = ('device', 'parent_id', 'rssi', 'last_updated')
+
+@admin.register(Battery)
+class BatteryAdmin(admin.ModelAdmin):
+    list_display = ('device', 'battery_percentage', 'timestamp')
